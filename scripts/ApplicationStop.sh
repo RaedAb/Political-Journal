bdfb23c9ab88d6a24fa29a1557fffc7cca52a2c4
Raed <abuzaidraed13@gmail.com>
2024-03-21 10:57:46 -0400

    check if app is running before stopping

--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# Stop the Node app
-pm2 stop political-journal
+# Check if the process exists before attempting to stop it
+if pm2 list | grep -q 'political-journal'; then
+    # If the process exists, stop it
+    pm2 stop political-journal
+    echo "Stopped the political-journal process."
+else
+    echo "The political-journal process is not running."
+fi
