32c7ad143320a103b2eb2ef34ab7419a916c1d57
Raed <abuzaidraed13@gmail.com>
2024-03-21 11:44:53 -0400

    added debug statements to script to check environement

--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Print environment variables
+echo "Environment variables:"
+env
+
+# Check current working directory
+echo "Current working directory: $(pwd)"
+
+# Check the user running the script
+echo "User running the script: $(whoami)"
+
 # Check if the process exists before attempting to stop it
 if pm2 list | grep -q 'political-journal'; then
     # If the process exists, stop it
