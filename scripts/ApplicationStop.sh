a0be62843276a9970c5e5b2bf831898674711886
Raed <abuzaidraed13@gmail.com>
2024-03-21 12:23:58 -0400

    Undid changes from last commit

--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -3,7 +3,7 @@
 # Check if the process exists before attempting to stop it
 if pm2 list | grep -q 'political-journal'; then
     # If the process exists, stop it
-    pm2 stop --silent political-journal
+    pm2 stop political-journal
     echo "Stopped the political-journal process."
 else
     echo "The political-journal process is not running."
