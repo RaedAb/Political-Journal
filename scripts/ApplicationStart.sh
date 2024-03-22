01989e623d7de331055b1f9f25c5f179a1023f13
Raed <abuzaidraed13@gmail.com>
2024-03-22 03:53:09 -0400

    Updated to check if process exists first

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Navigate to the application directory
-cd ~
+cd /var/www/Political-Journal
 
-# Start the Node.js application using PM2
-pm2 start political-journal
+# Check if the PM2 process with the name "political-journal" exists
+if pm2 info political-journal &> /dev/null; then
+    # If it exists, start the process
+    pm2 start political-journal
+else
+    # If it doesn't exist, start the application with the specified name
+    pm2 start app.js --name "political-journal"
+fi
